bd33843d145287b56688efeda883c3a040c0e210
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2024-08-14 16:41:02 +0900

    RecordApp : 03. 권한요청하기

--- app/src/main/java/com/jomiroid/recoderapp/MainActivity.kt
+++ app/src/main/java/com/jomiroid/recoderapp/MainActivity.kt
@@ -3,25 +3,105 @@ package com.jomiroid.recoderapp
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.media.MediaPlayer
-import android.media.MediaRecorder
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.jomiroid.recoderapp.databinding.ActivityMainBinding
-import java.io.IOException
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val REQUEST_AUDIO_CODE = 200
+    }
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.recordButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.RECORD_AUDIO
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // 권한이 있기 떄문에 실제로 녹음을 시작하면 됨
+                }
+
+                ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.RECORD_AUDIO
+                ) -> {
+                    // 권한이 없고 교육용 UI가 필요한 경우
+                    showPermissionRationalDialog()
+                }
+
+                else -> {
+                    // 권한 없고 바로 권한을 요청할 경우
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.RECORD_AUDIO),
+                        REQUEST_AUDIO_CODE
+                    )
+                }
+            }
+        }
+    }
+
+    private fun showPermissionRationalDialog() {
+        AlertDialog.Builder(this)
+            .setMessage(this.getString(R.string.requestAudioPermissionMessage))
+            .setPositiveButton(this.getString(R.string.requestAudioPermissionPositive)) { _, _ ->
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.RECORD_AUDIO),
+                    REQUEST_AUDIO_CODE
+                )
+            }
+            .setNegativeButton(this.getString(R.string.requestAudioPermissionNegative)) { dialogInterface, _ -> dialogInterface.cancel() }
+            .show()
+    }
+
+    private fun showPermissionSettingDialog() {
+        AlertDialog.Builder(this)
+            .setMessage(this.getString(R.string.requestAudioPermissionSettingMessage))
+            .setPositiveButton(this.getString(R.string.requestAudioPermissionSettingPositive)) { _, _ ->
+                navigateToAppSetting()
+            }
+            .setNegativeButton(this.getString(R.string.requestAudioPermissionSettingNegative)) { dialogInterface, _ -> dialogInterface.cancel() }
+            .show()
+    }
+
+    private fun navigateToAppSetting() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
+        }
+        startActivity(intent  )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted = requestCode == REQUEST_AUDIO_CODE
+                && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if (audioRecordPermissionGranted) {
+            // todo 녹음작업 시작
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.RECORD_AUDIO
+            )) {
+                showPermissionRationalDialog()
+            } else {
+                showPermissionSettingDialog()
+            }
+        }
     }
 }
